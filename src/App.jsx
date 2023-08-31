52b58142fb06e7d2854fbad6d8e6216e0ce6b098
Ayush-Dabas <aydbs23@gmail.com>
2023-08-31 17:43:58 +0530

    accesing the input using react usestate

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+
+  const handleInput = (e) => {
+    setInputText(e.target.value);
+  };
+  return (
+    <div className="App">
+      <h1>TextUtils - Word Counter, Character Counter, Remove Extra Space</h1>
+      <main className="inputArea">
+        <h3>Enter Your Text Here: </h3>
+        <input
+          type="text"
+          onChange={handleInput}
+          placeholder="Text"
+          value={inputText}
+        />
+        <button className="functionalBtns">Convert to Uppercase</button>
+        <button className="functionalBtns">Convert to Lowercase</button>
+        <button className="functionalBtns">Clear Text</button>
+        <button className="functionalBtns">Copy Text</button>
+        <button className="functionalBtns">Remove Extra Spaces</button>
+      </main>
+      <section className="summary">
+        <h3>Summary of your text</h3>
+        <p id="numWord">Number of words: #number</p>
+        <p id="numChar">Number of Characters: #char</p>
+        <p id="readTime">Reading Time: #time</p>
+      </section>
+      <section className="display">{inputText}</section>
+    </div>
+  );
+}
+
+export default App;
