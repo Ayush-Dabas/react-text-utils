cfc3f754c6236421d5dc696cf82635b2d80c1245
Ayush-Dabas <aydbs23@gmail.com>
2023-08-31 17:19:17 +0530

    started app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello world !</div>;
+  return (
+    <div className="App">
+      <h1>TextUtils - Word Counter, Character Counter, Remove Extra Space</h1>
+    </div>
+  );
 }
 
 export default App;
