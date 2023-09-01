e59dc9d8b29b8b320a875af730e695f11e906bb2
Ayush-Dabas <aydbs23@gmail.com>
2023-09-01 19:40:48 +0530

    added bootstrap in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [inputText, setInputText] = useState("");
